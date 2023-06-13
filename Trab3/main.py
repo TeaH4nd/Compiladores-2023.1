76ecb7e4fb0313edc6f4fdb53135dfa94e653a56
TeaH4nd <alexchamon@poli.ufrj.br>
2023-06-13 20:57:51 -0300

    fix bug no interpreter

--- Trab3/main.py
+++ Trab3/main.py
@@ -1,11 +1,13 @@
 import sys
 
-from lexer import Lexer
-from parser import Parser
+from lexer import Lexer, TokenType
+from myParser import Parser
 from interpreter import Interpreter
 
 def main():
     print('Digite \'q\' para sair!')
+    # Criação do interpretador
+    interpreter = Interpreter()
     while True:
         # Leitura da expressão aritmética prefixada a partir da entrada padrão
         try:
@@ -22,16 +24,13 @@ def main():
         # Criação do parser
         parser = Parser(lexer)
 
-        if parser.current_token:
-            if parser.current_token.token_type == TokenType.TokEOF:
+        if parser.currentToken:
+            if parser.currentToken.tokenType == TokenType.TokEOF:
                 print('End of File.')
                 break
             try:
                 # Análise sintática
                 syntax_tree = parser.parse()
-
-                # Criação do interpretador
-                interpreter = Interpreter()
 
                 # Avaliação da árvore sintática
                 interpreter.interpret(syntax_tree)
